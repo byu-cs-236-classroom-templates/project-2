6235f8c41ccbb727d5935f0ef186b95bab18510d
Eric Mercer <egm@cs.byu.edu>
2025-09-19 23:05:10 +0000

    Configure pylance to ignore systems files
    Closes #8

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 set -e
 
-# Install the package
-pip install --user --editable ".[dev]"
+# Upgrade pip in the base interpreter
+python -m pip install --upgrade pip
 
-# Install pre-commit
+# Remove old virtual environment
+rm -rf .venv
+
+# Create & activate venv
+python -m venv .venv
+
+# POSIX "source"
+. .venv/bin/activate
+
+# Upgrade pip in the venv
+python -m pip install --upgrade pip
+
+# Install your project (dev extras) into the venv (NO --user)
+pip install --editable ".[dev]"
+
+# Ensure pre-commit is available (either in [dev] or install here)
+# pip install pre-commit  # uncomment if not in [dev]
 pre-commit install
