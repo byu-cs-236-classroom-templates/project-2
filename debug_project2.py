a0eb8ee098792abe0e9aceb5ec4b962c34cde255
Eric Mercer <egm@cs.byu.edu>
2026-09-25 18:43:51 +0000

    Bump ruff to a newer version in pre-commit
    Closes #32

--- debug_project2.py
+++ debug_project2.py
@@ -1,8 +1,8 @@
-import sys  # noqa
+import sys
 
 sys.argv = ["project2", "arg1"]
 
-from project2.project2 import project2cli  # noqa
+from project2.project2 import project2cli
 
 if __name__ == "__main__":
     project2cli()
